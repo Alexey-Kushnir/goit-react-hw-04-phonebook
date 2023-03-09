2aa17ad2726a5897ae1210d4e6dc4f58e05b1525
Alexey-Kushnir <al7787@gmail.com>
2023-03-09 10:18:21 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -30,7 +30,7 @@ export const App = () => {
       return alert(`${name} is already in contacts.`);
     }
 
-    setContacts(() => [...contacts, { id: nanoid(8), name, number }]);
+    setContacts([...contacts, { id: nanoid(8), name, number }]);
     return resetForm();
   };
 
